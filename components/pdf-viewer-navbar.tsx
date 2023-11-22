4b089c0f82642f6e58f83125ecc16f9c00350482
pmcb99 <paulmcbrien10@gmail.com>
2023-11-22 23:14:35 +0000

    nav pdf movile

--- components/pdf-viewer-navbar.tsx
+++ components/pdf-viewer-navbar.tsx
@@ -3,46 +3,38 @@ import { Button } from "./ui/button";
 import { TopicButton } from "./topic-button";
 
 export function Nav({
-    pageNumber,
-    numPages,
-    pdfName,
-    topicComponent,
-    topicComponentProps,
-  }: {
-    pageNumber: number;
-    numPages: number;
-    pdfName: string;
-    topicComponent: any;
-    topicComponentProps: any
-  }) {
-    
-    const TopicComponent = topicComponent;
+  pageNumber,
+  numPages,
+  pdfName,
+  topicComponent,
+  topicComponentProps,
+}: {
+  pageNumber: number;
+  numPages: number;
+  pdfName: string;
+  topicComponent: any;
+  topicComponentProps: any;
+}) {
+  const TopicComponent = topicComponent;
 
-    return (
-      <nav className="bg-primary w-full h-24 rounded-xl">
+  return (
+    <nav className="flex md:flex-row flex-col bg-primary w-full md:h-24 rounded-xl items-center justify-center text-center gap-y-2 h-auto md:py-0 py-4">
+      <div>
+        <p className="text-2xl font-bold text-white dark:text-black">
+          {pdfName.replace("_", " ")}
+        </p>
+      </div>
 
-        <div className="mx-auto px-2 sm:px-6 lg:px-8 h-full flex flex-col justify-center">
-
-          <div className="relative flex h-16 items-center justify-between text-center">
-            <div className="flex flex-1 items-center justify-center ">
-              <div className="flex flex-col flex-shrink-0 items-center justify-center">
-                <p className="text-2xl font-bold tracking-tighter text-white dark:text-black">
-                  {pdfName.replace("_", " ")}
-                </p>
-              </div>
-
-              <TopicButton topicComponent={TopicComponent} topicComponentProps={topicComponentProps}/>
-            </div>
-            {/* <div className="flex items-center ">
+      <TopicButton
+        topicComponent={TopicComponent}
+        topicComponentProps={topicComponentProps}
+      />
+      {/* <div className="flex items-center ">
               <div className="bg-primary/60 text-black rounded-md px-3 py-2 text-sm font-medium"> */}
-                {/* <span>{pageNumber}</span> */}
-                {/* <span className="text-black-400"> / {numPages}</span> */}
-              {/* </div> */}
-            {/* </div> */}
-          </div>
-
-        </div>
-
-      </nav>
-    );
-  }
+      {/* <span>{pageNumber}</span> */}
+      {/* <span className="text-black-400"> / {numPages}</span> */}
+      {/* </div> */}
+      {/* </div> */}
+    </nav>
+  );
+}
